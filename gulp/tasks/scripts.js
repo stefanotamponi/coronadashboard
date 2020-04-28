b98c14c278ffa0e4ed79af5d71ecdcedac40bacf
Stefano <stefanotamponi@outlook.it>
2020-04-29 00:45:52 +0200

    first commit - adapted previous sandbox

---
+++ gulp/tasks/scripts.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+webpack = require('webpack');
+
+gulp.task('scripts', gulp.series('modernizr', function(callback) {
+  webpack(require('../../webpack.config.js'), function(err, stats) {
+    if (err) {
+      console.log(err.toString());
+    }
+
+    console.log(stats.toString());
+    callback();
+  });
+})); 
